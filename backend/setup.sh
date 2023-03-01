da37accf9d494726e036f19232052251c1d683e6
dzeleniak <zeleniak.david@gmail.com>
2023-02-28 16:50:26 -0800

    Add readme and setup

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,11 +1,3 @@
-echo "Get: gin"
-
-go get -u github.com/gin-gonic/gin
-
-echo "Get: GoDotEnv"
-
-go get github.com/joho/godotenv
-
 echo "Get: CompileDaemon"
 
 go get github.com/githubnemo/CompileDaemon
