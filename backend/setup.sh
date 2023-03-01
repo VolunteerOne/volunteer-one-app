765830429b9d01f62a66b70611319fe04c1b476e
dzeleniak <zeleniak.david@gmail.com>
2023-02-28 16:48:56 -0800

    Initialize Project

---
+++ backend/setup.sh
@@ -0,0 +1,15 @@
+echo "Get: gin"
+
+go get -u github.com/gin-gonic/gin
+
+echo "Get: GoDotEnv"
+
+go get github.com/joho/godotenv
+
+echo "Get: CompileDaemon"
+
+go get github.com/githubnemo/CompileDaemon
+
+echo "Install: CompileDaemon"
+
+go install github.com/githubnemo/CompileDaemon
