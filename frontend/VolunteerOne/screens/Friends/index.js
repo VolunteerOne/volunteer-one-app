066c5074272424f6c26098d0486d672d009e0cb6
mattacy <115319660+mattacy@users.noreply.github.com>
2023-03-05 12:38:59 -0800

    Create notifications sample constants
    added notifications.js as sample notifications for notification cards / notifications screen

--- frontend/VolunteerOne/screens/Friends/index.js
+++ frontend/VolunteerOne/screens/Friends/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { StyleSheet, Dimensions, ScrollView } from 'react-native';
-import { Block, theme } from 'galio-framework';
+// import { Block, theme } from 'galio-framework';
 
 import { Card } from '../../components';
 import articles from '../../constants/articles';
+import notifications from '../../constants/notifications';
+
+// <matt>
+import { Block, Button, Text, theme } from "galio-framework";
+// </matt>
+
 const { width } = Dimensions.get('screen');
 
 class Home extends React.Component {
@@ -12,18 +18,29 @@ class Home extends React.Component {
       <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.articles}>
-        <Block flex>
-          <Card item={articles[0]} horizontal  />
-          <Block flex row>
-            <Card item={articles[1]} style={{ marginRight: theme.SIZES.BASE }} />
-            <Card item={articles[2]} />
-          </Block>
-          <Card item={articles[3]} horizontal />
-          <Card item={articles[4]} full />
+
+        {/* <matt> */}
+        <Block>
+          <Text>
+            This is a test to add text item
+          </Text>
         </Block>
+        {/* </matt> */}
+
+        <Block flex>
+          <Card item={notifications[0]} horizontal  />
+          <Block flex row>
+            <Card item={notifications[1]} style={{ marginRight: theme.SIZES.BASE }} />
+            <Card item={notifications[2]} />
+          </Block>
+          <Card item={notifications[3]} horizontal />
+          <Card item={notifications[4]} full />
+        </Block>
+    
       </ScrollView>
     )
   }
+
 
   render() {
     return (
@@ -32,6 +49,8 @@ class Home extends React.Component {
       </Block>
     );
   }
+
+
 }
 
 const styles = StyleSheet.create({
