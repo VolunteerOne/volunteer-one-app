08f1628e243a748c63a1bc4e97ca81a6210ae8c1
mattacy <115319660+mattacy@users.noreply.github.com>
2023-03-05 12:43:36 -0800

    added NotificationsItem.js
    Added notification card component to file, based off of card component.

--- frontend/VolunteerOne/screens/Friends/index.js
+++ frontend/VolunteerOne/screens/Friends/index.js
@@ -1,31 +1,23 @@
 import React from 'react';
 import { StyleSheet, Dimensions, ScrollView } from 'react-native';
-// import { Block, theme } from 'galio-framework';
+import { Block, theme } from 'galio-framework';
 
 import { Card } from '../../components';
 // import articles from '../../constants/articles';
 import notifications from '../../constants/notifications';
 
 // <matt>
-import { Block, Button, Text, theme } from "galio-framework";
+// import { Block, Button, Text, theme } from "galio-framework";
 // </matt>
 
 const { width } = Dimensions.get('screen');
 
 class Home extends React.Component {
-  renderArticles = () => {
+  renderNotifications = () => {
     return (
       <ScrollView
         showsVerticalScrollIndicator={false}
         contentContainerStyle={styles.notifications}>
-
-        {/* <matt> */}
-        <Block>
-          <Text>
-            This is a test to add text item
-          </Text>
-        </Block>
-        {/* </matt> */}
 
         <Block flex>
           <Card item={notifications[0]} horizontal  />
@@ -45,7 +37,7 @@ class Home extends React.Component {
   render() {
     return (
       <Block flex center style={styles.home}>
-        {this.renderArticles()}
+        {this.renderNotifications()}
       </Block>
     );
   }
