4ae5e2de83aed0628a8f2b9551e1b999de01813a
mattacy <115319660+mattacy@users.noreply.github.com>
2023-03-05 15:06:22 -0800

    remove link to view more and change to time (hours ago)

--- frontend/VolunteerOne/components/NotificationItem.js
+++ frontend/VolunteerOne/components/NotificationItem.js
@@ -36,7 +36,7 @@ class NotificationItem extends React.Component {
         <TouchableWithoutFeedback onPress={() => navigation.navigate('Pro')}>
           <Block flex space="between" style={styles.cardDescription}>
             <Text size={14} style={styles.cardTitle}>{item.title}</Text>
-            <Text size={12} muted={!ctaColor} color={ctaColor || argonTheme.COLORS.ACTIVE} bold>{item.cta}</Text>
+            <Text size={12} muted={!ctaColor} color={ctaColor || argonTheme.COLORS.ACTIVE} bold>{item.time}h ago</Text>
           </Block>
         </TouchableWithoutFeedback>
       </Block>
