9d1db18742fdefa5f9ea31b54965d97d9c2d1e50
Cera <cerasamson@gmail.com>
2023-03-06 16:29:34 -0800

    Added stock images + profile photos :-)

--- frontend/VolunteerOne/constants/posts.js
+++ frontend/VolunteerOne/constants/posts.js
@@ -2,8 +2,16 @@ export default [
     {
       author: 'John Doe',
       date: '2 hr. ago',  // TODO: change date to dynamic
-      profileImage: 'https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80',
+      profileImage: 'https://images.pexels.com/photos/9324374/pexels-photo-9324374.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       horizontal: true
+    },
+    {
+      author: 'Jane Doe',
+      date: '30 min. ago',  // TODO: change date to dynamic
+      profileImage: 'https://images.pexels.com/photos/6994744/pexels-photo-6994744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      horizontal: true,
+      image: 'https://images.pexels.com/photos/5029859/pexels-photo-5029859.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
     },
   ];
