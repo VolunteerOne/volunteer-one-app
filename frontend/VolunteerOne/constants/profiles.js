6f78bafcf038a0750b7ff23c38589de6d518b1ba
Carmen Lee <carmen.lee.lv@gmail.com>
2023-03-07 20:12:16 -0800

    created constants file to populate profile screen (volunteer)

---
+++ frontend/VolunteerOne/constants/profiles.js
@@ -0,0 +1,29 @@
+export default {
+  name: "Jessica Jones",
+  image:
+    "https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?fit=crop&w=1650&q=80",
+  age: "23",
+  city: "San Francisco",
+  country: "USA",
+  description:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porttitor, leo non eleifend scelerisque, tellus nisi rhoncus mi, sit amet imperdiet ipsum orci a metus. Integer eu rutrum erat.",
+  hours: "50",
+  friends: "10",
+  following: "80",
+  recentActivity: [
+    {
+      date: 'January 22, 2023',
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porttitor, leo non eleifend scelerisque, tellus nisi rhoncus mi, sit amet imperdiet ipsum orci a metus. Integer eu rutrum erat.",
+      likes: "1",
+      comments: "1",
+    },
+    {
+      date: 'January 20, 2023',
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porttitor, leo non eleifend scelerisque, tellus nisi rhoncus mi, sit amet imperdiet ipsum orci a metus. Integer eu rutrum erat.",
+      likes: "2",
+      comments: "2",
+    },
+  ],
+};
