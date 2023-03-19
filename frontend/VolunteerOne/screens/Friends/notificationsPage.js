dd3ee93003933c33eb8a6bf6781010713d4d377d
mattacy <115319660+mattacy@users.noreply.github.com>
2023-03-19 11:39:53 -0700

    export default NotificationsPage
    change export default from home to NotificationsPage
    
    Change class name to NotificationsPage
    
    Remove comments

--- frontend/VolunteerOne/screens/Friends/notificationsPage.js
+++ frontend/VolunteerOne/screens/Friends/notificationsPage.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import { StyleSheet, Dimensions, ScrollView } from 'react-native';
 import { Block, theme } from 'galio-framework';
-import { Button } from "../../components";
-
-// import { Card } from '../../components';
 import NotificationItem from '../../components/NotificationItem';
-// import articles from '../../constants/articles';
 import notifications from '../../constants/notifications';
 
-// <matt>
-// import { Block, Button, Text, theme } from "galio-framework";
-// </matt>
 
 const { width } = Dimensions.get('screen');
 
-class Home extends React.Component {
+class NotificationsPage extends React.Component {
   renderNotifications = () => {
     return (
       <ScrollView
@@ -27,12 +20,7 @@ class Home extends React.Component {
           <NotificationItem item={notifications[2]} horizontal />
           <NotificationItem item={notifications[3]} horizontal />
           <NotificationItem item={notifications[4]} horizontal />
-          {/* <Block flex row>
-            <NotificationItem item={notifications[1]} style={{ marginRight: theme.SIZES.BASE }} />
-            <NotificationItem item={notifications[2]} />
-          </Block> */}
-          {/* <NotificationItem item={notifications[3]} horizontal /> */}
-          {/* <NotificationItem item={notifications[4]} full /> */}
+
         </Block>
     
       </ScrollView>
@@ -61,4 +49,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default Home;
+export default NotificationsPage;
