b6fc1375cd3921e8cbfa1f06e7646a87e18a2596
mattacy <115319660+mattacy@users.noreply.github.com>
2023-03-23 00:21:33 -0700

    complete exit button
    add exit.png (x icon) and add to modal.
    
    additional styling, also

--- frontend/VolunteerOne/components/NewAnnouncementModal.js
+++ frontend/VolunteerOne/components/NewAnnouncementModal.js
@@ -4,7 +4,7 @@
 // it works like a popup.
 
 import React from 'react';
-import {Alert, Modal, StyleSheet, Pressable, View, Dimensions, TextInput} from 'react-native';
+import {Alert, Modal, StyleSheet, Pressable, View, Dimensions, TextInput, Image} from 'react-native';
 import { Block, Text, theme } from 'galio-framework';
 import { Images, argonTheme } from "../constants";
 
@@ -28,55 +28,61 @@ class NewAnouncementModal extends React.Component {
             Alert.alert('Modal has been closed.');
             this.setState({modalVisible: !modalVisible});
           }}>
+             
+
           <View style={styles.centeredView}>    
             <View style={styles.modalView}>
-              <Block style={styles.header}>
-
+              {/* x icont to exit modal */}
+              <Pressable
+                onPress={() => this.setState({modalVisible: !modalVisible})}
+                style={{alignItems: 'flex-end'}}>
+                <Image
+                style={styles.exit}
+                source={require('../assets/imgs/exit.png')} />
+              </Pressable>
+              
+              <View style={styles.modalViewInside}>
+   
                 <Text style={styles.header}>
                   New Announcement 
-                  <Pressable
-                      onPress={() => this.setState({modalVisible: !modalVisible})}>
-                      <Text style={styles.exit}>x</Text>
-                  </Pressable>
                 </Text>
-              </Block>
+     
 
-              <Text style={styles.secondaryHeader}>
-                Post title
-              </Text>
+                <Text style={styles.secondaryHeader}>
+                  Post title
+                </Text>
+                <Block width={width * 0.8 - 20} style={{ marginBottom: 15 }}>
+                  <TextInput
+                    style={styles.input} 
+                    placeholder="Enter a title"
+                    placeholderTextColor={"lightgrey"}                  
+                    // onChangeText={handleInput}
+                  />
+                </Block>
 
-              <Block width={width * 0.8 - 20} style={{ marginBottom: 15 }}>
-                <TextInput
-                  style={styles.input} 
-                  placeholder="Enter a title"
-                  placeholderTextColor={"lightgrey"}                  
-                  // onChangeText={handleInput}
-                />
-              </Block>
+                <Text style={styles.secondaryHeader}>
+                  Description
+                </Text>
+                <Block width={width * 0.8 - 20}  style={{ marginBottom: 15 }}>
+                  <TextInput
+                    style={styles.input}
+                    placeholder="Provide announcement details here"
+                    placeholderTextColor={"lightgrey"}
+                    height={height * 0.3}
+                    textAlignVertical={'top'}
+                    paddingTop={10}
+                    multiline={true}
+                    // onChangeText={handleInput}
+                  />
+                </Block>
 
-              <Text style={styles.secondaryHeader}>
-                Description
-              </Text>
-              <Block width={width * 0.8 - 20}  style={{ marginBottom: 15 }}>
-                <TextInput
-                  style={styles.input}
-                  placeholder="Provide announcement details here"
-                  placeholderTextColor={"lightgrey"}
-                  height={height * 0.3}
-                  textAlignVertical={'top'}
-                  paddingTop={10}
-                  multiline={true}
-                  // onChangeText={handleInput}
-                />
-              </Block>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => this.setState({modalVisible: !modalVisible})}>
+                  <Text style={styles.textStyle}>CREATE ANNOUNCEMENT</Text>
+                </Pressable>
 
-
-              <Pressable
-                style={[styles.button, styles.buttonClose]}
-                onPress={() => this.setState({modalVisible: !modalVisible})}>
-                <Text style={styles.textStyle}>CREATE ANNOUNCEMENT</Text>
-              </Pressable>
-
+              </View>
             </View>
           </View>
         </Modal>
@@ -93,18 +99,19 @@ class NewAnouncementModal extends React.Component {
 
 const styles = StyleSheet.create({
   exit: {
-    // borderRadius: 20,
-    // padding: 10,
-    fontWeight: 'bold',
-    elevation: 2,
-    
+    margin:10,
+    marginBottom: 0,
+    justifyContent:"flex-end",
+    width: 15,
+    height: 15,
   },
+
   header: {
     fontSize: 25,
     fontWeight: 'bold',
     color: "#525F7F",
-    marginBottom: 10,
-    marginTop: 10,
+    marginBottom: 20,
+    // marginTop: 10,
   },
   secondaryHeader: {
     fontSize: 17,
@@ -128,21 +135,11 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
    
   },
-
-  // matt's added styles above ^^^
-
-
-  centeredView: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginTop: 22,
-  },
   modalView: {
     margin: 20,
     backgroundColor: 'white',
     // borderRadius: 5,
-    padding: 35,
+    // padding: 35,
     paddingTop: 0,
     // alignItems: 'center',
     shadowColor: '#000',
@@ -150,9 +147,36 @@ const styles = StyleSheet.create({
       width: 0,
       height: 2,
     },
-    shadowOpacity: 0.25,
+    shadowOpacity: 0.1,
     shadowRadius: 4,
     elevation: 5,
+  },
+
+  // matt's added styles above ^^^
+
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+
+  modalViewInside: {
+    // margin: 20,
+    // backgroundColor: 'white',
+    // // borderRadius: 5,
+    padding: 25,
+    paddingTop: 0,
+    // paddingTop: 0,
+    // // alignItems: 'center',
+    // shadowColor: '#000',
+    // shadowOffset: {
+    //   width: 0,
+    //   height: 2,
+    // },
+    // shadowOpacity: 0.25,
+    // shadowRadius: 4,
+    // elevation: 5,
   },
   button: {
     borderRadius: 5,
