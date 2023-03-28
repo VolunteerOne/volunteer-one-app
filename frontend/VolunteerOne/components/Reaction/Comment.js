e46b5cd0ac89982f60caa5962568f37660cb50d2
Cera <cerasamson@gmail.com>
2023-03-27 22:57:52 -0700

    Add icons to Like/Comment

--- frontend/VolunteerOne/components/Reaction/Comment.js
+++ frontend/VolunteerOne/components/Reaction/Comment.js
@@ -2,6 +2,7 @@
 import React, { useState }  from "react";
 import { Block, Button, Text, Input, theme} from "galio-framework";
 import { StyleSheet, TextInput, View } from "react-native";
+import Icon from "../Icon";
 
 const Comment = () => {
         const [show, setShow] = useState(false);
@@ -11,7 +12,17 @@ const Comment = () => {
             <Button
                 style={styles.button}
                 onPress={() => setShow(!show)}>
+                
+                <Block flex row>
+                <Icon
+      family="MaterialIcons"
+      size={20}
+      name="comment"
+      color="#32325D"
+    />
                 <Text style={styles.titleText}>Comment</Text>
+                </Block>
+
           </Button>
 
           { show ? 
@@ -22,7 +33,6 @@ const Comment = () => {
             width: 360}}
             ></Input> 
                 : null }
-
 </View>
         );
 };
@@ -31,6 +41,7 @@ const styles = StyleSheet.create({
         fontSize: 18,
         color: "#32325D",
         textAlign: 'right',
+        marginLeft: 7,
       },
     button: {
         borderless: true,
@@ -39,7 +50,7 @@ const styles = StyleSheet.create({
         backgroundColor: '#fff',
         width: 'auto',
         height: 20,
-        marginLeft: 160,
+        marginLeft: 125,
         marginTop: -30,
 
     }
--- frontend/VolunteerOne/components/Reaction/LikeButton.js
+++ frontend/VolunteerOne/components/Reaction/LikeButton.js
@@ -3,6 +3,7 @@ import React from "react";
 import { Block, Button, Text } from "galio-framework";
 import { StyleSheet, Image } from "react-native";
 import { Svg } from "react-native-svg";
+import Icon from "../Icon";
 
 class LikeButton extends React.Component {
     state = {
@@ -21,8 +22,16 @@ class LikeButton extends React.Component {
 
         return (
             <Button style={styles.button}
-            onPress={incrementLikes} >        
+            onPress={incrementLikes} >      
+      <Block flex row>
+                <Icon
+      family="MaterialIcons"
+      size={20}
+      name="thumb-up"
+      color="#32325D"
+    />
             <Text style={styles.titleText}>Likes: {this.state.count}</Text>
+            </Block>
           </Button>
 
         );
@@ -33,6 +42,7 @@ const styles = StyleSheet.create({
     titleText: {
         fontSize: 18,
         color: "#32325D",
+        marginLeft: 10,
       },
     button: {
         borderless: true,
