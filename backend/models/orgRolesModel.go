331567e72d1bb6cf66078125857fc4067c8812af
Dillon <dillongharder@gmail.com>
2023-03-28 15:41:01 -0700

    Add foreign key relationship to org roles model

--- backend/models/orgRolesModel.go
+++ backend/models/orgRolesModel.go
@@ -4,12 +4,15 @@ import "gorm.io/gorm"
 
 type OrgRoles struct {
 	gorm.Model
-	AdminId 		uint `gorm:"not null"`
-	OrganizationId 	uint `gorm:"not null"`
-	Verified 		bool `gorm:"default:0;not null"`
-	
+	UsersID        uint `gorm:"not null"`
+	OrganizationID uint `gorm:"not null"`
+	Verified       bool `gorm:"default:0;not null"`
+
 	// Lower values take priority.
 	// We have leeway for additional roles.
-	// (0 = owner, 1 = manager, 10 = member) 
-	Role			uint `gorm:"default:5;not null"`
+	// (0 = owner, 1 = manager, 10 = member)
+	Role uint `gorm:"default:5;not null"`
+
+	Users        Users        `gorm:"foreignkey:UsersID"`
+	Organization Organization `gorm:"foreignkey:OrganizationID"`
 }
