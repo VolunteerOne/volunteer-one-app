fd5d2a29aeb458ef371b8f12800e65ef7e826d36
genaro <lopezbae@unlv.nevada.edu>
2023-03-28 20:52:57 -0700

    Fixed styling issues to match current templates

--- frontend/VolunteerOne/screens/Onboarding/NewPassword.js
+++ frontend/VolunteerOne/screens/Onboarding/NewPassword.js
@@ -36,53 +36,59 @@ const NewPassword = ({ navigation }) => {
     navigation.navigate('Login');
   };
 
-return (
+  return (
     <Block flex middle>
       <StatusBar hidden />
       <ImageBackground
         source={Images.RegisterBackground}
         style={{ width, height, zIndex: 1 }}
       >
-      <Block safe flex middle>
-      <Block style={styles.container}>
-      <Block flex={0.5} middle>
+        <Block safe flex middle>
+          <Block style={styles.loginContainer}>
+            <Block flex>
+              <Block flex={0.5} middle style={styles.instructionText}>
                 <Block center>
                   <Image source={Images.VolunteerOneIcon} style={styles.logo} />
                 </Block>
               </Block>
-        <Block style={styles.inputContainer}>
-          <Text style={styles.inputLabel}>New Password</Text>
-          <Block width={width * 0.8}>
-            <TextInput
-              style={styles.input}
-              secureTextEntry={true}
-              placeholder="New Password"
-              onChangeText={handleNewPasswordInput}
-            />
+              <Block flex center>
+                <KeyboardAvoidingView
+                  style={{ flex: 1 }}
+                  behavior="padding"
+                  enabled
+                >
+                  <Block width={width * 0.8} style={{ marginBottom: 15 }}>
+                    <TextInput
+                      secureTextEntry={true}
+                      style={styles.input}
+                      placeholder="New Password"
+                      onChangeText={handleNewPasswordInput}
+                    />
+                  </Block>
+                  <Block width={width * 0.8}>
+                    <TextInput
+                      secureTextEntry={true}
+                      style={styles.input}
+                      placeholder="Comfirm Password"
+                      onChangeText={handleConfirmPasswordInput}
+                    />
+                  </Block>
+                  <Block middle>
+                    <Button
+                      color="primary"
+                      style={styles.createButton}
+                      onPress={handleSaveBtnClick}
+                    >
+                      <Text bold size={14} color={argonTheme.COLORS.WHITE}>
+                        CREATE PASSWORD
+                      </Text>
+                    </Button>
+                  </Block>
+                </KeyboardAvoidingView>
+              </Block>
+            </Block>
           </Block>
         </Block>
-        <Block width={width * 0.8} style={styles.inputContainer}>
-          <Text style={styles.inputLabel}>Confirm Password</Text>
-          <Block width={width * 0.8}>
-            <TextInput
-              style={styles.input}
-              secureTextEntry={true}
-              placeholder="Confirm Password"
-              onChangeText={handleConfirmPasswordInput}
-            />
-          </Block>
-        </Block>
-        <Block middle>
-          <Button
-            style={styles.button}
-            onPress={handleSaveBtnClick}
-            color="#8898AA"
-          >
-            <Text style={styles.buttonText}>RESET PASSWORD</Text>
-          </Button>
-        </Block>
-      </Block>
-      </Block>
       </ImageBackground>
     </Block>
   );
@@ -90,7 +96,21 @@ return (
 // }
 
 const styles = StyleSheet.create({
-  container: {
+  input: {
+    borderColor: argonTheme.COLORS.BORDER,
+    height: 44,
+    backgroundColor: "#FFFFFF",
+    shadowColor: argonTheme.COLORS.BLACK,
+    shadowOffset: { width: 0, height: 1 },
+    shadowRadius: 2,
+    shadowOpacity: 0.05,
+    elevation: 2,
+    paddingLeft: 10,
+  },
+  instructionText: {
+    flexDirection: "row",
+  },
+  loginContainer: {
     width: width * 0.9,
     height: height * 0.75,
     backgroundColor: "#F4F5F7",
@@ -105,39 +125,24 @@ const styles = StyleSheet.create({
     elevation: 1,
     overflow: "hidden",
   },
-  inputContainer: {
-      marginVertical: 10,
+  inputIcons: {
+    marginRight: 12,
   },
-  inputLabel: {
-      marginBottom: 5,
-      fontWeight: "bold",
+  passwordCheck: {
+    paddingLeft: 15,
+    paddingTop: 13,
+    paddingBottom: 30,
   },
-  input: {
-    borderColor: argonTheme.COLORS.BORDER,
-    height: 44,
-    backgroundColor: "#FFFFFF",
-    shadowColor: argonTheme.COLORS.BLACK,
-    shadowOffset: { width: 0, height: 4 },
-    shadowRadius: 2,
-    shadowOpacity: 0.05,
-    elevation: 2,
-    paddingLeft: 10,
-    paddingTop:10,
-  },
-  button: {
-      width: width * 0.8,
-      marginTop: 25,
-      backgroundColor: argonTheme.COLORS.PRIMARY,
-  },
-  buttonText: {
-      color: "#FFFFFF",
+  createButton: {
+    width: width * 0.8,
+    marginTop: 25,
   },
   logo: {
-      width: 265,
-      height: 50,
-      zIndex: 2,
-      position: 'relative',
-      marginTop: '20%',
+    width: 265,
+    height: 50,
+    zIndex: 2,
+    position: 'relative',
+    marginTop: '20%'
   },
 });
 
