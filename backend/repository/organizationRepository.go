091c37aa384c0b2337f7b23648f5db3a1dfa9ada
dzeleniak <zeleniak.david@gmail.com>
2023-04-10 19:56:02 -0700

    Add organiztion repostiory

---
+++ backend/repository/organizationRepository.go
@@ -0,0 +1,80 @@
+package repository
+
+import (
+	"errors"
+
+	"github.com/VolunteerOne/volunteer-one-app/backend/models"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type OrganizationRepository interface {
+	CreateOrganization(models.Organization) (models.Organization, error)
+	GetOrganizations() ([]models.Organization, error)
+	GetOrganizationById(uuid.UUID) (models.Organization, error)
+	UpdateOrganization(uuid.UUID, models.Organization) (models.Organization, error)
+	DeleteOrganization(uuid.UUID) (error)
+}
+
+type organizationRepository struct {
+	DB *gorm.DB
+}
+
+func NewOrganizationRepository(db *gorm.DB) organizationRepository {
+	return organizationRepository {
+		DB: db,
+	}
+}
+
+func (r organizationRepository) CreateOrganization(org models.Organization) (models.Organization, error) {
+	result := r.DB.Create(&org)
+
+	if result.Error != nil {
+		return models.Organization{}, errors.New("creation Failed")
+	}
+
+	return org, nil
+}
+
+func (r organizationRepository) GetOrganizations() ([]models.Organization, error) {
+	var orgs []models.Organization
+	result := r.DB.Find(&orgs)
+
+	if result.Error != nil {
+		return []models.Organization{}, errors.New("could not retrieve organizations")
+	}
+
+	return orgs, nil
+}
+
+func (r organizationRepository) GetOrganizationById(id uuid.UUID) (models.Organization, error) {
+	var org models.Organization
+
+	result := r.DB.First(&org, id) 
+
+	if result.Error != nil {
+		return models.Organization{}, errors.New("could not retrieve organization")
+	}
+
+	return org, nil
+}
+
+
+func (r organizationRepository) UpdateOrganization(org models.Organization) (models.Organization, error) {
+	result := r.DB.Save(&org)
+
+	if result.Error != nil { 
+		return models.Organization{}, errors.New("could not update organization")
+	}
+
+	return org, nil
+}
+
+func (r organizationRepository) DeleteOrganization(org models.Organization) (error) {
+	result := r.DB.Delete(&org)
+	if result.Error != nil {
+		return errors.New("could not delete organization")
+	}
+
+	return nil
+}
