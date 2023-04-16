e24f75f1e965971ed8f3d4ee2b5816594a51fbf6
mattacy <115319660+mattacy@users.noreply.github.com>
2023-04-15 22:31:56 -0700

    Update profile.js
    add all users

--- frontend/VolunteerOne/constants/ProfileTab/profile.js
+++ frontend/VolunteerOne/constants/ProfileTab/profile.js
@@ -188,6 +188,382 @@ export default {
     ],
   },
 
+  "Doug Salas" : {
+    name: "Jane Hollis",
+    userType: "volunteer",
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+    age: "29",
+    city: "Las Vegas",
+    country: "USA",
+    description:
+      "Adventurous soul, travel enthusiast, and coffee addict ☕️ | Passionate about capturing moments through my lens 📸 | Advocate for women's empowerment 💪 | #Wanderlust #Photography #GirlPower",
+    hours: "50",
+    friends: {
+      value: 5,
+      friendsList: [
+        {
+          username: "Jessica Jones",
+          image:
+            "https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?fit=crop&w=1650&q=80",
+        },
+        {
+          username: "Doug Salas",
+          image:
+            "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          username: "Aliya Diwa",
+          image:
+            "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          username: "Noah Fernandez",
+          image:
+            "https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80",
+        },
+        {
+          username: "Bryce Hansen",
+          image:
+            "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=400&q=60",
+          horizontal: true,
+        },
+      ],
+    },
+    following: {
+      value: 3,
+      followingList: [
+        {
+          username: "Habitat for Humanity",
+          image:
+            "https://e7.pngegg.com/pngimages/903/572/png-clipart-old-colony-habitat-for-humanity-volunteering-donation-habitat-for-humanity-moncton-area-charity-logo-text-hand.png",
+        },
+        {
+          username: "Global Volunteers",
+          image:
+            "https://yt3.googleusercontent.com/ytc/AL5GRJU2cI52r0t8YZXC_4LtTJHT8-jitUP6Pq_w59b1=s900-c-k-c0x00ffffff-no-rj",
+        },
+        {
+          username: "Global Humanitarian Aid",
+          image:
+            "https://media.licdn.com/dms/image/C4E03AQEZpW_HHOoyoQ/profile-displayphoto-shrink_800_800/0/1591471142424?e=2147483647&v=beta&t=T03fQtmGiueXyCRLoRdEvWMWEIWTrPS5kiYCtEHub9o",
+        },
+      ],
+    },
+    recentActivity: [
+      {
+        date: "January 22, 2023",
+        description:
+          "Just finished an volunteer session at the gym, teaching kids how to dance! 💪 Feeling pumped and energized! #FitnessGoals #GymLife #HealthyLifestyle",
+        likes: "1",
+        comments: "1",
+      },
+      {
+        date: "January 20, 2023",
+        description:
+          "Feeling grateful and blessed to celebrate my 5th anniversary volunteering with American Running Society! 💕 Time flies when you're having fun! #VolunteeringWins #Anniversary #Blessed",
+        likes: "2",
+        comments: "2",
+      },
+    ],
+    upcomingEvents: [
+      {
+        date: "February 15, 2025",
+        title: "Help pack disaster relief bags",
+        image:
+          "https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80",
+      },
+      {
+        date: "March 15, 2025",
+        title: "Volunteer at the Soup Kitchen",
+        image:
+          "https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80",
+      },
+    ],
+  },
+
+  "Aliya Diwa" : {
+    name: "Jane Hollis",
+    userType: "volunteer",
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+    age: "29",
+    city: "Las Vegas",
+    country: "USA",
+    description:
+      "Adventurous soul, travel enthusiast, and coffee addict ☕️ | Passionate about capturing moments through my lens 📸 | Advocate for women's empowerment 💪 | #Wanderlust #Photography #GirlPower",
+    hours: "50",
+    friends: {
+      value: 5,
+      friendsList: [
+        {
+          username: "Jessica Jones",
+          image:
+            "https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?fit=crop&w=1650&q=80",
+        },
+        {
+          username: "Doug Salas",
+          image:
+            "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          username: "Aliya Diwa",
+          image:
+            "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          username: "Noah Fernandez",
+          image:
+            "https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80",
+        },
+        {
+          username: "Bryce Hansen",
+          image:
+            "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=400&q=60",
+          horizontal: true,
+        },
+      ],
+    },
+    following: {
+      value: 3,
+      followingList: [
+        {
+          username: "Habitat for Humanity",
+          image:
+            "https://e7.pngegg.com/pngimages/903/572/png-clipart-old-colony-habitat-for-humanity-volunteering-donation-habitat-for-humanity-moncton-area-charity-logo-text-hand.png",
+        },
+        {
+          username: "Global Volunteers",
+          image:
+            "https://yt3.googleusercontent.com/ytc/AL5GRJU2cI52r0t8YZXC_4LtTJHT8-jitUP6Pq_w59b1=s900-c-k-c0x00ffffff-no-rj",
+        },
+        {
+          username: "Global Humanitarian Aid",
+          image:
+            "https://media.licdn.com/dms/image/C4E03AQEZpW_HHOoyoQ/profile-displayphoto-shrink_800_800/0/1591471142424?e=2147483647&v=beta&t=T03fQtmGiueXyCRLoRdEvWMWEIWTrPS5kiYCtEHub9o",
+        },
+      ],
+    },
+    recentActivity: [
+      {
+        date: "January 22, 2023",
+        description:
+          "Just finished an volunteer session at the gym, teaching kids how to dance! 💪 Feeling pumped and energized! #FitnessGoals #GymLife #HealthyLifestyle",
+        likes: "1",
+        comments: "1",
+      },
+      {
+        date: "January 20, 2023",
+        description:
+          "Feeling grateful and blessed to celebrate my 5th anniversary volunteering with American Running Society! 💕 Time flies when you're having fun! #VolunteeringWins #Anniversary #Blessed",
+        likes: "2",
+        comments: "2",
+      },
+    ],
+    upcomingEvents: [
+      {
+        date: "February 15, 2025",
+        title: "Help pack disaster relief bags",
+        image:
+          "https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80",
+      },
+      {
+        date: "March 15, 2025",
+        title: "Volunteer at the Soup Kitchen",
+        image:
+          "https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80",
+      },
+    ],
+  },
+
+  "Noah Fernandez" : {
+    name: "Jane Hollis",
+    userType: "volunteer",
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+    age: "29",
+    city: "Las Vegas",
+    country: "USA",
+    description:
+      "Adventurous soul, travel enthusiast, and coffee addict ☕️ | Passionate about capturing moments through my lens 📸 | Advocate for women's empowerment 💪 | #Wanderlust #Photography #GirlPower",
+    hours: "50",
+    friends: {
+      value: 5,
+      friendsList: [
+        {
+          username: "Jessica Jones",
+          image:
+            "https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?fit=crop&w=1650&q=80",
+        },
+        {
+          username: "Doug Salas",
+          image:
+            "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          username: "Aliya Diwa",
+          image:
+            "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          username: "Noah Fernandez",
+          image:
+            "https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80",
+        },
+        {
+          username: "Bryce Hansen",
+          image:
+            "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=400&q=60",
+          horizontal: true,
+        },
+      ],
+    },
+    following: {
+      value: 3,
+      followingList: [
+        {
+          username: "Habitat for Humanity",
+          image:
+            "https://e7.pngegg.com/pngimages/903/572/png-clipart-old-colony-habitat-for-humanity-volunteering-donation-habitat-for-humanity-moncton-area-charity-logo-text-hand.png",
+        },
+        {
+          username: "Global Volunteers",
+          image:
+            "https://yt3.googleusercontent.com/ytc/AL5GRJU2cI52r0t8YZXC_4LtTJHT8-jitUP6Pq_w59b1=s900-c-k-c0x00ffffff-no-rj",
+        },
+        {
+          username: "Global Humanitarian Aid",
+          image:
+            "https://media.licdn.com/dms/image/C4E03AQEZpW_HHOoyoQ/profile-displayphoto-shrink_800_800/0/1591471142424?e=2147483647&v=beta&t=T03fQtmGiueXyCRLoRdEvWMWEIWTrPS5kiYCtEHub9o",
+        },
+      ],
+    },
+    recentActivity: [
+      {
+        date: "January 22, 2023",
+        description:
+          "Just finished an volunteer session at the gym, teaching kids how to dance! 💪 Feeling pumped and energized! #FitnessGoals #GymLife #HealthyLifestyle",
+        likes: "1",
+        comments: "1",
+      },
+      {
+        date: "January 20, 2023",
+        description:
+          "Feeling grateful and blessed to celebrate my 5th anniversary volunteering with American Running Society! 💕 Time flies when you're having fun! #VolunteeringWins #Anniversary #Blessed",
+        likes: "2",
+        comments: "2",
+      },
+    ],
+    upcomingEvents: [
+      {
+        date: "February 15, 2025",
+        title: "Help pack disaster relief bags",
+        image:
+          "https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80",
+      },
+      {
+        date: "March 15, 2025",
+        title: "Volunteer at the Soup Kitchen",
+        image:
+          "https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80",
+      },
+    ],
+  },
+
+  "Bryce Hansen" : {
+    name: "Jane Hollis",
+    userType: "volunteer",
+    image:
+      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1287&q=80",
+    age: "29",
+    city: "Las Vegas",
+    country: "USA",
+    description:
+      "Adventurous soul, travel enthusiast, and coffee addict ☕️ | Passionate about capturing moments through my lens 📸 | Advocate for women's empowerment 💪 | #Wanderlust #Photography #GirlPower",
+    hours: "50",
+    friends: {
+      value: 5,
+      friendsList: [
+        {
+          username: "Jessica Jones",
+          image:
+            "https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?fit=crop&w=1650&q=80",
+        },
+        {
+          username: "Doug Salas",
+          image:
+            "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          username: "Aliya Diwa",
+          image:
+            "https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+        },
+        {
+          username: "Noah Fernandez",
+          image:
+            "https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80",
+        },
+        {
+          username: "Bryce Hansen",
+          image:
+            "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=400&q=60",
+          horizontal: true,
+        },
+      ],
+    },
+    following: {
+      value: 3,
+      followingList: [
+        {
+          username: "Habitat for Humanity",
+          image:
+            "https://e7.pngegg.com/pngimages/903/572/png-clipart-old-colony-habitat-for-humanity-volunteering-donation-habitat-for-humanity-moncton-area-charity-logo-text-hand.png",
+        },
+        {
+          username: "Global Volunteers",
+          image:
+            "https://yt3.googleusercontent.com/ytc/AL5GRJU2cI52r0t8YZXC_4LtTJHT8-jitUP6Pq_w59b1=s900-c-k-c0x00ffffff-no-rj",
+        },
+        {
+          username: "Global Humanitarian Aid",
+          image:
+            "https://media.licdn.com/dms/image/C4E03AQEZpW_HHOoyoQ/profile-displayphoto-shrink_800_800/0/1591471142424?e=2147483647&v=beta&t=T03fQtmGiueXyCRLoRdEvWMWEIWTrPS5kiYCtEHub9o",
+        },
+      ],
+    },
+    recentActivity: [
+      {
+        date: "January 22, 2023",
+        description:
+          "Just finished an volunteer session at the gym, teaching kids how to dance! 💪 Feeling pumped and energized! #FitnessGoals #GymLife #HealthyLifestyle",
+        likes: "1",
+        comments: "1",
+      },
+      {
+        date: "January 20, 2023",
+        description:
+          "Feeling grateful and blessed to celebrate my 5th anniversary volunteering with American Running Society! 💕 Time flies when you're having fun! #VolunteeringWins #Anniversary #Blessed",
+        likes: "2",
+        comments: "2",
+      },
+    ],
+    upcomingEvents: [
+      {
+        date: "February 15, 2025",
+        title: "Help pack disaster relief bags",
+        image:
+          "https://images.unsplash.com/photo-1516559828984-fb3b99548b21?ixlib=rb-1.2.1&auto=format&fit=crop&w=2100&q=80",
+      },
+      {
+        date: "March 15, 2025",
+        title: "Volunteer at the Soup Kitchen",
+        image:
+          "https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80",
+      },
+    ],
+  },
+
   // organizations
   "Habitat for Humanity" : {
     name: "Habitat for Humanity",
